da46d5915bc71dbc05e3594763adc533ee53338e
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2025-06-14 21:26:35 +0300

    setup tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,16 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint"],
+  css: ["~/assets/css/main.css"],
+  modules: ["@nuxt/eslint", "@nuxt/icon"],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
   eslint: {
     config: {
       standalone: false,
