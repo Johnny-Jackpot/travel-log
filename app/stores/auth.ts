9405f08f74cf1c28b95e536a8cf15c4d291121e8
Oleksandr Nazarenko <sanya.nazarenko.up036@gmail.com>
2025-08-20 23:54:14 +0300

    tl-20 (#61)

--- app/stores/auth.ts
+++ app/stores/auth.ts
@@ -3,9 +3,15 @@ import { createAuthClient } from "better-auth/vue";
 const authClient = createAuthClient();
 
 export const useAuthStore = defineStore("useAuthStore", () => {
-  const session = authClient.useSession();
-  const user = computed(() => session.value.data?.user);
-  const loading = computed(() => session.value.isPending || session.value.isRefetching);
+  const session = ref<Awaited<ReturnType<typeof authClient.useSession>> | null>(null);
+
+  async function init() {
+    const data = await authClient.useSession(useFetch);
+    session.value = data;
+  }
+
+  const user = computed(() => session.value?.data?.user);
+  const loading = computed(() => session.value?.isPending);
 
   async function signIn() {
     await authClient.signIn.social({
@@ -19,5 +25,5 @@ export const useAuthStore = defineStore("useAuthStore", () => {
     await authClient.signOut();
   }
 
-  return { loading, signIn, signOut, user };
+  return { init, loading, signIn, signOut, user };
 });
