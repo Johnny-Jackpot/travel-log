0781c12370ae350b5bc19c3872631da408b909bb
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2025-08-22 00:57:14 +0300

    removed unused code

--- app/components/sidebar-button.vue
+++ app/components/sidebar-button.vue
@@ -9,33 +9,15 @@ const route = useRoute();
 </script>
 
 <template>
-  <div class="tooltip tooltip-right" :data-tip="props.label">
-    <NuxtLink
-      class="flex w-64 justify-start gap-2 p-2 hover:bg-base-300 hover:cursor-pointer flex-nowrap"
-      :to="props.href" :class="route.path === href && 'bg-base-200'"
-    >
-      <Icon :name="props.icon" size="24" />
-      {{ props.label }}
-    </NuxtLink>
-  </div>
+  <NuxtLink
+    class="flex w-64 justify-start gap-2 p-2 hover:bg-base-300 hover:cursor-pointer flex-nowrap"
+    :to="props.href" :class="route.path === href && 'bg-base-200'"
+  >
+    <Icon :name="props.icon" size="24" />
+    {{ props.label }}
+  </NuxtLink>
 </template>
 
 <style scoped>
-.grow-enter-active {
-  animation: grow 0.1s;
-}
 
-.grow-leave-active {
-  animation: grow 0.1s reverse;
-}
-
-@keyframes grow {
-  0% {
-    transform: scale(0);
-  }
-
-  100% {
-    transform: scale(1);
-  }
-}
 </style>
