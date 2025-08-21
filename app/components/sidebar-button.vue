ecbdaa0003977179b042b7b5a99080c7b70e8efc
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2025-08-21 23:37:34 +0300

    added sidebar

---
+++ app/components/sidebar-button.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const props = defineProps<{
+  label: string;
+  icon: string;
+  href: string;
+}>();
+
+const route = useRoute();
+</script>
+
+<template>
+  <NuxtLink
+    :to="props.href"
+    :class="{ 'bg-base-200': route.path === href }"
+    class="flex justify-start gap-2 p-2 hover:bg-base-300 hover:cursor-pointer"
+  >
+    <Icon :name="props.icon" size="24" />
+    {{ props.label }}
+  </NuxtLink>
+</template>
+
+<style scoped>
+
+</style>
--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -3,7 +3,20 @@
 </script>
 
 <template>
-  <h1>You are logged in</h1>
+  <div class="flex-1 flex">
+    <div class="bg-base-100 w-64">
+      <div class="flex justify-end">
+        <Icon name="tabler:chevron-left" size="42" />
+      </div>
+      <div class="flex flex-col">
+        <SidebarButton label="Locations" icon="tabler:map" href="/dashboard" />
+        <SidebarButton label="Add Location" icon="tabler:circle-plus-filled" href="/dashboard/add" />
+        <div class="divider" />
+        <SidebarButton label="Sign Out" icon="tabler:logout-2" href="/sign-out" />
+      </div>
+    </div>
+    <div class="flex-1" />
+  </div>
 </template>
 
 <style scoped>
