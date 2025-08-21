fa642f123cbe5741a67532c3f649ce0cd05e08e7
Oleksandr Nazarenko <sanya.nazarenko.up036@gmail.com>
2025-08-22 02:01:01 +0300

    Tl 20 1 (#65)
    * tl-20
    
    * added sidebar
    
    * updated sidebar
    
    * added transition
    
    * made smooth open closed animation
    
    * simplified sidebar

--- app/components/sidebar-button.vue
+++ app/components/sidebar-button.vue
@@ -10,20 +10,19 @@ const route = useRoute();
 </script>
 
 <template>
-  <div
-    class="tooltip tooltip-right"
-    :data-tip="props.showLabel ? undefined : props.label"
-  >
+  <div class="tooltip tooltip-right" :data-tip="props.showLabel ? undefined : props.label">
     <NuxtLink
       :to="props.href"
-      :class="[
-        route.path === href && 'bg-base-200',
-        props.showLabel ? 'justify-start' : 'justify-center',
-      ]"
-      class="flex  gap-2 p-2 hover:bg-base-300 hover:cursor-pointer"
+      :class="route.path === href && 'bg-base-200'"
+      class="flex justify-start gap-2 p-2 hover:bg-base-300 hover:cursor-pointer flex-nowrap"
     >
-      <Icon :name="props.icon" size="24" />
-      <span v-show="props.showLabel">{{ props.label }}</span>
+      <Icon :name="props.icon" size="24" class="flex-shrink-0" />
+      <span
+        class="whitespace-nowrap transition-all duration-300"
+        :class="props.showLabel ? 'opacity-100 w-auto' : 'opacity-0 w-0'"
+      >
+        {{ props.label }}
+      </span>
     </NuxtLink>
   </div>
 </template>
