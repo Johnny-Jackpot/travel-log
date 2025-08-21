0082cf0528c3dcd60d90c655862d8e5d61fa47de
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2025-08-21 23:55:47 +0300

    updated sidebar

--- app/components/sidebar-button.vue
+++ app/components/sidebar-button.vue
@@ -3,20 +3,29 @@ const props = defineProps<{
   label: string;
   icon: string;
   href: string;
+  showLabel: boolean;
 }>();
 
 const route = useRoute();
 </script>
 
 <template>
-  <NuxtLink
-    :to="props.href"
-    :class="{ 'bg-base-200': route.path === href }"
-    class="flex justify-start gap-2 p-2 hover:bg-base-300 hover:cursor-pointer"
+  <div
+    class="tooltip tooltip-right"
+    :data-tip="props.showLabel ? undefined : props.label"
   >
-    <Icon :name="props.icon" size="24" />
-    {{ props.label }}
-  </NuxtLink>
+    <NuxtLink
+      :to="props.href"
+      :class="[
+        route.path === href && 'bg-base-200',
+        props.showLabel ? 'justify-start' : 'justify-center',
+      ]"
+      class="flex  gap-2 p-2 hover:bg-base-300 hover:cursor-pointer"
+    >
+      <Icon :name="props.icon" size="24" />
+      <span v-show="props.showLabel">{{ props.label }}</span>
+    </NuxtLink>
+  </div>
 </template>
 
 <style scoped>
--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -1,18 +1,40 @@
 <script setup lang="ts">
-
+const isSidebarOpen = ref(true);
 </script>
 
 <template>
   <div class="flex-1 flex">
-    <div class="bg-base-100 w-64">
-      <div class="flex justify-end">
-        <Icon name="tabler:chevron-left" size="42" />
+    <div class="bg-base-100" :class="isSidebarOpen ? 'w-64' : 'w-16'">
+      <div
+        :class="isSidebarOpen ? 'justify-end' : 'justify-center'"
+        class="flex hover:cursor-pointer hover:bg-base-200"
+        @click="isSidebarOpen = !isSidebarOpen"
+      >
+        <Icon
+          :name="isSidebarOpen ? 'tabler:chevron-left' : 'tabler:chevron-right'"
+          size="42"
+        />
       </div>
       <div class="flex flex-col">
-        <SidebarButton label="Locations" icon="tabler:map" href="/dashboard" />
-        <SidebarButton label="Add Location" icon="tabler:circle-plus-filled" href="/dashboard/add" />
+        <SidebarButton
+          :show-label="isSidebarOpen"
+          label="Locations"
+          icon="tabler:map"
+          href="/dashboard"
+        />
+        <SidebarButton
+          :show-label="isSidebarOpen"
+          label="Add Location"
+          icon="tabler:circle-plus-filled"
+          href="/dashboard/add"
+        />
         <div class="divider" />
-        <SidebarButton label="Sign Out" icon="tabler:logout-2" href="/sign-out" />
+        <SidebarButton
+          :show-label="isSidebarOpen"
+          label="Sign Out"
+          icon="tabler:logout-2"
+          href="/sign-out"
+        />
       </div>
     </div>
     <div class="flex-1" />
