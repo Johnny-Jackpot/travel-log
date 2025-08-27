2196764e38b94b93523645478405f8bcd98a5425
Oleksandr Nazarenko <sanya.nazarenko.up036@gmail.com>
2025-08-27 23:53:40 +0300

    Tl 24 (#67)
    * tl-24
    
    * tl-24
    
    * added unique slug

--- server/api/locations.post.ts
+++ server/api/locations.post.ts
@@ -1,5 +1,7 @@
 import db from "~~/lib/db";
 import { InsertLocation, location } from "~~/lib/db/schema";
+import { customAlphabet } from "nanoid";
+import slugify from "slug";
 
 export default defineEventHandler(async (event) => {
   if (!event.context?.user) {
@@ -32,10 +34,25 @@ export default defineEventHandler(async (event) => {
     }));
   }
 
+  const baseSlug = slugify(result.data.name, { lower: true });
+  const nanoid = customAlphabet("1234567890abcdefghijklmnopqrstuvwxyz");
+  const slug = `${baseSlug}-${Date.now()}-${nanoid(5)}`;
+
+  const existing = await db.query.location.findFirst({
+    where: (loc, { eq }) => eq(loc.slug, slug),
+  });
+  if (existing) {
+    return sendError(event, createError({
+      statusCode: 409,
+      statusMessage: "Location with this name already exists.",
+      data: { name: "Location name must be unique." },
+    }));
+  }
+
   const [createdLocation] = await db.insert(location).values({
     ...result.data,
     userId: event.context.user.id,
-    slug: result.data.name.replaceAll(" ", "-").toLowerCase(),
+    slug,
   }).returning();
 
   return createdLocation;
