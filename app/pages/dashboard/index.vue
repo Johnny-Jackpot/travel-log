7bad5f6593a75270cbc44f6cae82b5fc7442a119
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2025-09-05 18:51:13 +0300

    added locations fetching

--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
-
+const { data, status } = await useFetch("/api/locations", {
+  lazy: true,
+});
 </script>
 
 <template>
@@ -7,7 +9,13 @@
     <h2 class="text-2xl">
       Locations
     </h2>
-    <div class="flex flex-col gap-2 mt-4">
+    <div v-if="status === 'pending'">
+      <span class="loading loading-spinner loading-xl" />
+    </div>
+    <div v-else-if="data">
+      <pre>{{ JSON.stringify(data, null, 2) }}</pre>
+    </div>
+    <div v-else class="flex flex-col gap-2 mt-4">
       <p>Add a location to get started</p>
       <NuxtLink to="/dashboard/add" class="btn btn-primary w-40">
         <Icon name="tabler:circle-plus-filled" size="24" />
--- server/api/locations.get.ts
+++ server/api/locations.get.ts
@@ -1,3 +1,5 @@
-export default defineAuthenticatedEventHandler(async () => {
-  return { success: true };
+import { findLocations } from "~~/lib/db/queries/location";
+
+export default defineAuthenticatedEventHandler(async (event) => {
+  return await findLocations(event.context.user.id);
 });
