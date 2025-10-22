96d3ec7cd68512b34d3e024aa32a6feb2ad199ce
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2025-10-23 00:38:19 +0300

    added light and dark styles for map

--- app/components/app/map.client.vue
+++ app/components/app/map.client.vue
@@ -2,8 +2,7 @@
 // const style = "https://api.maptiler.com/maps/bright-v2/style.json?key=dt1Vuq0B2XZkQVL3jHXV";
 // const style = "/styles/dark-map.json";
 const style = "/styles/light-map.json";
-
-const center = [30.559482, 48.21322];
+const center: [number, number] = [30.559482, 48.21322];
 const zoom = 4;
 </script>
 
